5c3b81a8f0ea6094d40246d0816dc8647c881a37
nathan@tribeca-it.com <nathan@tribeca-it.com>
2018-11-23 13:53:44 +0000

    Another c

--- NGame/Source/Graphics/GraphicsHelper.cs
+++ NGame/Source/Graphics/GraphicsHelper.cs
@@ -10,6 +10,7 @@ namespace NGame.Source.Graphics
 {
     public static class GraphicsHelper
     {
+        private static Camera _TempCam = new Camera();
         private static Texture2D _Texture = new Texture2D(Globals.graphicsDevice.GraphicsDevice, 1, 1);
         public static void DrawRectangle(float x, float y, float width, float height, Color color)
         {
@@ -23,15 +24,34 @@ namespace NGame.Source.Graphics
         /// <param name="camera">The Camera Entity</param>
         /// <param name="entity">The Entity to be drawn</param>
         /// <returns></returns>
-        public static float CalculateDrawnAbsoluteX(Camera camera, Entity entity)
+        public static float DrawnAbsoluteX(Camera camera, Entity entity)
         {
             float xval = ((entity.Position.X * Globals.Scale) - (camera.Position.X * Globals.Scale))
-                - 0f;
-            return 0f;
+                + GetOffSet(camera).X;
+            return xval;
+        }
+        public static float DrawnAbsoluteY(Camera camera, Entity entity)
+        {
+            float xval = ((entity.Position.Y * Globals.Scale) - (camera.Position.Y * Globals.Scale))
+                + (GetOffSet(camera).Y * Globals.Scale);
+            return xval;
+        }
+        public static float DrawnAbsoluteX(Entity entity)
+        {
+            
+            float xval = ((entity.Position.X * Globals.Scale) - (_TempCam.Position.X * Globals.Scale))
+                + GetOffSet(_TempCam).X;
+            return xval;
+        }
+        public static float DrawnAbsoluteY(Entity entity)
+        {
+            float xval = ((entity.Position.Y * Globals.Scale) - (_TempCam.Position.Y * Globals.Scale))
+                + (GetOffSet(_TempCam).Y * Globals.Scale);
+            return xval;
         }
         public static Vector2 GetOffSet(Entity entity)
         {
-            return new Vector2(entity.Position.X / 2, entity.Position.Y / 2);
+            return new Vector2(entity.Dimension.X / 2, entity.Dimension.Y / 2);
         }
     }
 }
